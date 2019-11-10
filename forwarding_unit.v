9d71f3780f9934d37cc64e6190f81dc8c493153a
Mustafa Mahmoud <mustafamohammed_auc@aucegypt.edu>
2019-11-10 21:04:42 +0200

    .

--- forwarding_unit.v
+++ forwarding_unit.v
@@ -10,20 +10,20 @@ output reg [1:0]forwardB);
 
 always @(*) begin
 
-forwardA = 2'b00;
-forwardB = 2'b00;
+	forwardA = 2'b00;
+	forwardB = 2'b00;
 
-if(MEM_WB_regwrite && (MEM_WB_Rd !=0) && (MEM_WB_Rd == ID_EX_Rs1) && (EX_MEM_Rd == ID_EX_Rs1) && !(EX_MEM_regwrite!=0 && EX_MEM_Rd !=0))
-forwardA = 2'b01;
+	if(MEM_WB_regwrite && (MEM_WB_Rd !=0) && (MEM_WB_Rd == ID_EX_Rs1) && (EX_MEM_Rd == ID_EX_Rs1) && !(EX_MEM_regwrite!=0 && EX_MEM_Rd !=0))
+		forwardA = 2'b01;
 
-if(MEM_WB_regwrite &&( MEM_WB_Rd !=0) && (MEM_WB_Rd == ID_EX_Rs2) && (EX_MEM_Rd == ID_EX_Rs2) && !(EX_MEM_regwrite!=0 && EX_MEM_Rd !=0))
-forwardB   = 2'b01;
+	if(MEM_WB_regwrite &&( MEM_WB_Rd !=0) && (MEM_WB_Rd == ID_EX_Rs2) && (EX_MEM_Rd == ID_EX_Rs2) && !(EX_MEM_regwrite!=0 && EX_MEM_Rd !=0))
+		forwardB = 2'b01;
 
-if(EX_MEM_regwrite &&( EX_MEM_Rd !=0) && (EX_MEM_Rd == ID_EX_Rs1))
-forwardA = 2'b10;
+	if(EX_MEM_regwrite &&( EX_MEM_Rd !=0) && (EX_MEM_Rd == ID_EX_Rs1))
+		forwardA = 2'b10;
 
-if(EX_MEM_regwrite && (EX_MEM_Rd !=0) && (EX_MEM_Rd == ID_EX_Rs2))
-forwardB = 2'b10;
+	if(EX_MEM_regwrite && (EX_MEM_Rd !=0) && (EX_MEM_Rd == ID_EX_Rs2))
+		forwardB = 2'b10;
 
 end
 endmodule
