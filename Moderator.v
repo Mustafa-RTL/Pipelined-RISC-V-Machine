910429f4478ad5a27b5d414acc7e9dd65ff9971d
Mustafa-RTL <mustafamohammed_auc@aucegypt.edu>
2019-11-10 06:45:10 +0200

    a7la Moderator :*

--- Moderator.v
+++ Moderator.v
@@ -1,12 +1,17 @@
-module Moderator(input clk,
-	output x1);
+module Moderator(
+	input clk,
+	output tick_tock);
+
 reg x;
+
 initial begin 
 x = 0;
 end
 
 always @ (posedge(clk)) begin
-x = ~x // x = 1 clk 2 x = 0 clk 1
+x = ~x; // x = 1 clk 2 x = 0 clk 1
 end
-assign x1 = x;
+
+assign tick_tock = x;
+
 endmodule
