41d1f9552b91d962f51fe6f77fe8804cdae734c4
HaithamWafa <49447918+HaithamWafa@users.noreply.github.com>
2019-11-10 20:31:15 +0200

    Forwarding Unit

---
+++ forwarding_unit.v
@@ -0,0 +1,29 @@
+module forwarding_unit(
+input [4:0]ID_EX_Rs1, 
+input [4:0]ID_EX_Rs2,
+input [4:0]EX_MEM_Rd,
+input [4:0] MEM_WB_Rd,
+input EX_MEM_regwrite,
+input MEM_WB_regwrite,
+output reg[1:0]forwardA,
+output reg [1:0]forwardB);
+
+always @(*) begin
+
+forwardA = 2'b00;
+forwardB = 2'b00;
+
+if(MEM_WB_regwrite && (MEM_WB_Rd !=0) && (MEM_WB_Rd == ID_EX_Rs1) && (EX_MEM_Rd == ID_EX_Rs1) && !(EX_MEM_regwrite!=0 && EX_MEM_Rd !=0))
+forwardA = 2'b01;
+
+if(MEM_WB_regwrite &&( MEM_WB_Rd !=0) && (MEM_WB_Rd == ID_EX_Rs2) && (EX_MEM_Rd == ID_EX_Rs2) && !(EX_MEM_regwrite!=0 && EX_MEM_Rd !=0))
+forwardB   = 2'b01;
+
+if(EX_MEM_regwrite &&( EX_MEM_Rd !=0) && (EX_MEM_Rd == ID_EX_Rs1))
+forwardA = 2'b10;
+
+if(EX_MEM_regwrite && (EX_MEM_Rd !=0) && (EX_MEM_Rd == ID_EX_Rs2))
+forwardB = 2'b10;
+
+end
+endmodule
